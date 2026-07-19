f8179887b293e4ffde82e305e467c180ecd34a6a
kevin-coder25 <khansaqib045@gmail.com>
2026-07-19 21:18:09 +0500

    enhancement

--- obj/Debug/net10.0/BankingSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/BankingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b3ac2f413e3d7f3e181990ace04e7f8145fd92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb8fb189c24ee226363d603248619cf06416dc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
